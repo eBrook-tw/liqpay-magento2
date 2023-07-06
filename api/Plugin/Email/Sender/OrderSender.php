a3d1270be23ba0aadd20e696b62d2731b79e5dea
kelly.feng <kelly.feng@ebrook.com.tw>
2023-07-06 14:29:24 +0800

    add 1.2.7

--- api/Plugin/Email/Sender/OrderSender.php
+++ api/Plugin/Email/Sender/OrderSender.php
@@ -2,17 +2,26 @@
 
 namespace Pronko\LiqPayApi\Plugin\Email\Sender;
 
+use Magento\Sales\Model\Order\Email\Sender;
+
+/**
+ * Sends order email to the customer.
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class OrderSender
 {
-    public function beforeSend(
+
+    public function aroundSend(
         \Magento\Sales\Model\Order\Email\Sender\OrderSender $subject,
+        callable $proceed,
         $order,
         $forceSyncMode = false
     ) {
         if ($order->getPayment()->getMethod() == 'pronko_liqpay' && $order->getState() == 'new') {
             return false;
+        } else {
+            return $proceed($order,$forceSyncMode);
         }
-
-        return $subject;
     }
 }
